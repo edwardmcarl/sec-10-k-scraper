120be6165ab7aced45fa5b6a92dd6ffc37c3f6a8
Sena Yevenyo <you@example.com>
2022-03-27 22:35:48 -0400

    fix: fix regex bugs

--- backend/parser/parser.py
+++ backend/parser/parser.py
@@ -1,10 +1,12 @@
 # Reason for escaping mypy type check: https://bugs.launchpad.net/beautifulsoup/+bug/1843791
 # Can create a 'stublist' but wanted to get this commit first
+import gzip
 import re
 from typing import Dict
+from urllib.error import HTTPError, URLError
+from urllib.request import Request, urlopen
 
-import pandas as pd # type: ignore
-import requests # type: ignore
+import pandas as pd  # type: ignore
 from bs4 import BeautifulSoup  # type: ignore
 
 
@@ -13,8 +15,10 @@ class ParserError(Exception):
     DOCUMENT_NOT_SUPPORTED = "Parsing for this document is not supported"
     SERVER_ERROR = "The SEC EDGAR server could not process the request"
     CONNECTION_ERROR = "The application failed to reach the server"
+    UNEXPECTED_ERROR = "Something occured when decompressing and/or decoding response from SEC EDGAR server"
+    NO_FILE_EXISTS_ERROR = "The file requested does not exist in SEC EDGAR database"
 
-    def __init__(self, message: str, *values: object) -> None:
+    def __init__(self, message: str, *values: object, originalError=None) -> None:
         self.message = message
         super().__init__(self.message)
 
@@ -23,7 +27,8 @@ class Parser:
     HTML5LIB = 0
     HTML_PARSER = 1
     LXML = 2
-    FIELD_REGEX = r"((>(I){0,1}(tem|TEM)(\s|&#160;|&nbsp;)|ITEM(\s|&#160;|&nbsp;))(1|1A|2|3|4|5|6|7|7A|8|9|9A|9B|10|11|12|13|14|15))\.{0,1}"
+    FIELD_REGEX = r"((>(I){0,1}(tem|TEM)(\s|&#160;|&nbsp;)|ITEM(\s|&#160;|&nbsp;))(1(A|B|0|1|2|3|4|5|6){0,1}|2|3|4|5|6|7|7A|8|9(A|B){0,1}))\.{0,1}"
+
     FIELDS = [
         "item1",
         "item1a",
@@ -39,7 +44,7 @@ class Parser:
 
     def parse_document(
         self, document_url: str, parser_tool: int = LXML
-    ) -> Dict[str, str]:
+    ) -> Dict[str, Dict[str, str]]:
 
         # This logic is influenced by this GitHub gist: https://gist.github.com/anshoomehra/ead8925ea291e233a5aa2dcaa2dc61b2
         if parser_tool == Parser.HTML5LIB:
@@ -54,13 +59,42 @@ class Parser:
         if not (document_url.endswith(".txt")):
             raise ParserError(ParserError.DOCUMENT_NOT_SUPPORTED, document_url)
 
-        req = requests.get(document_url)
-        if req.status_code >= 400 and req.status_code < 500:
-            raise ParserError(ParserError.CONNECTION_ERROR)
-        elif req.status_code >= 500:
-            raise ParserError(ParserError.SERVER_ERROR)
+        # req = requests.get(document_url)
+        # if req.status_code >= 400 and req.status_code < 500:
+        #     raise ParserError(ParserError.CONNECTION_ERROR)
+        # elif req.status_code >= 500:
+        #     raise ParserError(ParserError.SERVER_ERROR)
+        hdrs = {
+            "Host": "www.sec.gov",
+            "User-Agent": "Lafayette College yevenyos@lafayette.edu",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept": "*/*",
+        }
+        req = Request(document_url, headers=hdrs, method="GET")
+        try:
+            with urlopen(req) as res:
+                data = res.read()
+                encoding = res.info().get_content_charset("utf-8")
+                # Decompressing received data
+                try:
+                    data = gzip.decompress(data)
+                    data = data.decode(encoding)
+                except Exception as e:
+                    raise ParserError(ParserError.UNEXPECTED_ERROR, originalError=e)
+        # HTTPError has to come before URLError. HTTPError is a subset of URLError
+        except HTTPError as e:
+            if e.code == 404:
+                raise ParserError(
+                    ParserError.NO_FILE_EXISTS_ERROR,
+                    document_url,
+                    originalError=e,
+                )
+            else:
+                raise ParserError(ParserError.SERVER_ERROR, originalError=e)
+        except URLError as f:
+            raise ParserError(ParserError.CONNECTION_ERROR, originalError=f)
 
-        raw_10k = req.text
+        raw_10k = data
         doc_start_pattern = re.compile(r"<DOCUMENT>")
         doc_end_pattern = re.compile(r"</DOCUMENT>")
 
@@ -80,34 +114,41 @@ class Parser:
 
         matches = regex.finditer(document["10-K"])
 
-        test_df = pd.DataFrame([(x.group(), x.start(), x.end()) for x in matches])
+        df = pd.DataFrame([(x.group(), x.start(), x.end()) for x in matches])
+        print(df)
 
-        test_df.columns = ["item", "start", "end"]
-        test_df["item"] = test_df.item.str.lower()
+        df.columns = ["item", "start", "end"]
+        df["item"] = df.item.str.lower()
 
-        test_df.replace("&#160;", " ", regex=True, inplace=True)
-        test_df.replace("&nbsp;", " ", regex=True, inplace=True)
-        test_df.replace(" ", "", regex=True, inplace=True)
-        test_df.replace("\\.", "", regex=True, inplace=True)
-        test_df.replace(">", "", regex=True, inplace=True)
-        test_df.replace("tem", "item", regex=True, inplace=True)
+        df.replace("&#160;", " ", regex=True, inplace=True)
+        df.replace("&nbsp;", " ", regex=True, inplace=True)
+        df.replace(" ", "", regex=True, inplace=True)
+        df.replace("\\.", "", regex=True, inplace=True)
+        df.replace(">", "", regex=True, inplace=True)
+        df.replace("item", "tem", regex=True, inplace=True)
+        df.replace("tem", "item", regex=True, inplace=True)
 
-        pos_dat = test_df.sort_values("start", ascending=True).drop_duplicates(
+        pos_df = df.sort_values("start", ascending=True).drop_duplicates(
             subset=["item"], keep="last"
         )
-        pos_dat.set_index("item", inplace=True)
+        pos_df.set_index("item", inplace=True)
+        print(pos_df)
         document_map = {}
-        index_length = len(pos_dat.index)
-        for index, item in enumerate(pos_dat.index):
+        index_length = len(pos_df.index)
+        for index, item in enumerate(pos_df.index):
             if item in Parser.FIELDS:
                 if index < index_length - 1:
                     text = document["10-K"][
-                        pos_dat["start"]
-                        .loc[item] : pos_dat["start"]
-                        .loc[pos_dat[index + 1]]
+                        pos_df["start"]
+                        .loc[item] : pos_df["start"]
+                        .loc[pos_df.index[index + 1]]
                     ]
                 else:
-                    text = document["10-K"][pos_dat["start"].loc[item] :]
-                document_map[item] = BeautifulSoup(text, parser).get_text("\n\n")
+                    text = document["10-K"][pos_df["start"].loc[item] :]
+                soup = BeautifulSoup(text, parser)
+                document_map[item] = {
+                    "html": soup.prettify(),
+                    "text": soup.get_text("\n\n"),
+                }
 
         return document_map
