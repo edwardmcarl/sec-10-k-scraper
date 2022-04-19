8e1eefc5d4f0115989f80fdf084c78f3aa70820a
Sena Yevenyo <you@example.com>
2022-04-19 16:06:34 -0400

    chore: minor fix

--- ui/test/specs/search_test.js
+++ ui/test/specs/search_test.js
@@ -3,7 +3,7 @@
 
 const assert = require('assert').strict;
 describe('FracTracker UI Search', async () => {
-  it('suggests centity names when the user enters a search key', async () => {
+  it('suggests entity names when the user enters a search key', async () => {
     const search_bar = await browser
       .react$$('Container')[1]
       .react$('FormGroup')
