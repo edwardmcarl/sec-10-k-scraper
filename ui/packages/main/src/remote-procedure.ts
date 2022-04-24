e0355937a5311ee0f78d96717b19cbb3a1b594c2
Edward Carl <55811915+edwardmcarl@users.noreply.github.com>
2022-04-24 17:43:58 -0400

    intermediate

--- ui/packages/main/src/remote-procedure.ts
+++ ui/packages/main/src/remote-procedure.ts
@@ -1,9 +1,18 @@
 import * as zerorpc from 'zerorpc';
 const client = new zerorpc.Client();
-client.connect('tcp://localhost:55555');
+let connected = false;
+
+export async function connectToBackend(port: string) {
+    client.connect(`tcp://localhost:${port}`);
+    connected = true;
+}
+
  //client.on("error", blah)
 
 export async function remoteCall(funcName:string, args: any[] = []) {
+    if (!connected){
+        throw new Error('Not connected to the backend!');
+    }
     return new Promise((resolve, reject) => {
         client.invoke(funcName, ...args, (error:any, response:any) => {
             if (error) {
